11ac3fa69f25090c4a8b0cf7e865264bf01bcbf0
NicolasMarcilloux <nmar33@free.fr>
2024-04-03 21:06:41 +0200

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# MinecraftExampleMod
+Base pour un mod minecraft via Fabric (v1.20.1)
---
+++ src/main/java/com/tartiflex/dofuscraft/DofuscraftClient.java
@@ -0,0 +1,11 @@
+package com.tartiflex.dofuscraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DofuscraftClient implements ClientModInitializer {
+
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
